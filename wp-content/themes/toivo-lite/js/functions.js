8f52ecd54b4bf4cb2fbdef3c6c9e31d20b76db2e
koabica <sadaharo9@gmail.com>
2020-07-17 21:32:05 +0100

    update

--- wp-content/themes/toivo-lite/js/functions.js
+++ wp-content/themes/toivo-lite/js/functions.js
@@ -103,24 +103,18 @@
 /**
  * Skip link focus fix.
  */
-(function() {
-	var isIe = /(trident|msie)/i.test( navigator.userAgent );
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-	if ( isIe && document.getElementById && window.addEventListener ) {
+	if ( ( is_webkit || is_opera || is_ie ) && document.getElementById && window.addEventListener ) {
 		window.addEventListener( 'hashchange', function() {
-			var id = location.hash.substring( 1 ),
-				element;
-
-			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
-				return;
-			}
-
-			element = document.getElementById( id );
+			var element = document.getElementById( location.hash.substring( 1 ) );
 
 			if ( element ) {
-				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
 					element.tabIndex = -1;
-				}
 
 				element.focus();
 			}
